5fabf4103e342accda30bd798411fa92352d3518
richard <chengli@buffalo.edu>
2025-08-09 00:18:53 -0400

    created a try test1 filefor testing of commmit and calling func from another file.

--- main.py
+++ main.py
@@ -3,12 +3,14 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 # test line
-# TEST
+
+import test1
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+print(test1.say_hi('Alice'))
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
